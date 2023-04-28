44556b2ae204ecf57f3910045969f5222c044e13
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-04-28 12:28:24 +0300

    Add recipient email address to reduce hard coding

--- SendMail/Models/Mail.cs
+++ SendMail/Models/Mail.cs
@@ -4,6 +4,7 @@ public class Mail
 {
     public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
+    public string RecipientEmail { get; set; } = string.Empty;
     public string? Subject { get; set; }
     public string Body { get; set; } = string.Empty;
 }
