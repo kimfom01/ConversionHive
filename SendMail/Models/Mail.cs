5689acc09307c59c9cf39a1649177101918365d1
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-04-28 12:14:06 +0300

    Add mail data model

---
+++ SendMail/Models/Mail.cs
@@ -0,0 +1,9 @@
+namespace SendMail.Models;
+
+public class Mail
+{
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string? Subject { get; set; }
+    public string Body { get; set; } = string.Empty;
+}
