1aa0e2a2828dcc11566140e68fb2e0adca140efc
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-06-02 00:43:44 +0300

    Create local mailer class for demo purposes

---
+++ SendMail/Services/LocalMailer.cs
@@ -0,0 +1,31 @@
+﻿using FluentEmail.Core;
+using FluentEmail.Core.Models;
+using FluentEmail.Smtp;
+using SendMail.Models;
+using System.Net.Mail;
+
+namespace SendMail.Services;
+
+public class LocalMailer : IMailer
+{
+    public async Task<SendResponse> SendMail(Mail mail)
+    {
+        var sender = new SmtpSender(() => new SmtpClient("localhost")
+        {
+            EnableSsl = true,
+            DeliveryMethod = SmtpDeliveryMethod.Network,
+            Port = 2525
+        });
+
+        Email.DefaultSender = sender;
+
+        var sendResponse = await Email
+            .From(mail.Email, mail.Name)
+            .To("myemail@mydomain.com")
+            .Subject(mail.Subject)
+            .Body(mail.Body)
+            .SendAsync();
+
+        return sendResponse;
+    }
+}
