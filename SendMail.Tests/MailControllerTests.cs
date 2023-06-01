6c6b2b4e6ca00d32322dd4052bc20419e792308b
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-06-02 02:37:24 +0300

    Add unit test project for controller logic

---
+++ SendMail.Tests/MailControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentEmail.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SendMail.Controllers;
+using SendMail.Models;
+using SendMail.Services;
+
+namespace SendMail.Tests;
+
+public class MailControllerTests
+{
+    private readonly Mock<IMailer> _mockMailer;
+    private readonly MailController _mailController;
+
+    public MailControllerTests()
+    {
+        _mockMailer = new Mock<IMailer>();
+        _mailController = new MailController(_mockMailer.Object);
+    }
+
+    [Fact]
+    public Task SendMail_WhenCalled_ReturnsOkResult()
+    {
+        return Task.CompletedTask;
+    }
+}
