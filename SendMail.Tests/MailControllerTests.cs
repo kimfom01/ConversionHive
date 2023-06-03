a242e5b63abfdecabc1ad76d993c13a44ef09123
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-06-03 11:02:55 +0300

    Add tests for controller actions Ok result and CreatedAtAction result failing

--- SendMail.Tests/MailControllerTests.cs
+++ SendMail.Tests/MailControllerTests.cs
@@ -1,5 +1,8 @@
-﻿using Moq;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
 using SendMail.Controllers;
+using SendMail.Models;
 using SendMail.Repository;
 using SendMail.Services;
 
@@ -17,8 +20,50 @@ public class MailControllerTests
     }
 
     [Fact]
-    public Task SendMail_WhenCalled_ReturnsOkResult()
+    public async Task SendMail_WhenCalled_ReturnsCreatedAtActionResult()
     {
-        return Task.CompletedTask;
+        var mail = new SendMailDto
+        {
+            Name = "Sender",
+            Email = "sender@sendmail.com",
+            RecipientEmail = "recipient@recipient.com",
+            Subject = "This is my subject",
+            Body = "This is the message body"
+        };
+        
+        var createdResponse = await _mailController.SendMail(mail);
+
+        Assert.IsType<CreatedAtActionResult>(createdResponse);
+    }
+
+    [Fact]
+    public async Task SendMail_WhenCalled_ReturnsBadRequestObjectResult()
+    {
+        var mail = new SendMailDto
+        {
+            RecipientEmail = "recipient@recipient.com",
+            Subject = "This is my subject",
+            Body = "This is the message body"
+        };
+
+        var badRequestResponse = await _mailController.SendMail(mail);
+
+        Assert.IsType<BadRequestObjectResult>(badRequestResponse);
+    }
+
+    [Fact]
+    public async Task GetSavedMail_WhenCalled_ReturnsOkObjectResult()
+    {
+        var okResult = await _mailController.GetSavedMail(1);
+
+        Assert.IsType<OkObjectResult>(okResult as OkObjectResult);
+    }
+    
+    [Fact]
+    public async Task GetSavedMail_WhenCalled_ReturnsNotFoundResult()
+    {
+        var notFound = await _mailController.GetSavedMail(3);
+
+        Assert.IsType<NotFoundResult>(notFound);
     }
 }
