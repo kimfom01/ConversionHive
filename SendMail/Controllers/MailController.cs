75dd8eabb4b2e5416b23a7a7bf0a2ecddd4a32b3
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-06-03 10:43:28 +0300

    Expand mail controller Add repository to save mail to db Add Get action to retrieve previously sent mails

--- SendMail/Controllers/MailController.cs
+++ SendMail/Controllers/MailController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using SendMail.Models;
+using SendMail.Repository;
 using SendMail.Services;
 
 namespace SendMail.Controllers;
@@ -9,22 +10,55 @@ namespace SendMail.Controllers;
 public class MailController : ControllerBase
 {
     private readonly IMailer _mailer;
+    private readonly IMailRepository _mailRepository;
 
-    public MailController(IMailer mailer)
+    public MailController(IMailer mailer, IMailRepository mailRepository)
     {
         _mailer = mailer;
+        _mailRepository = mailRepository;
     }
 
     [HttpPost]
-    public async Task<IActionResult> SendMail(Mail mail)
+    public async Task<IActionResult> SendMail(SendMailDto sendMail)
     {
-        var sendResponse = await _mailer.SendMail(mail);
+        var mailToSend = MapToMailObject(sendMail);
+        
+        var sendResponse = await _mailer.SendMail(mailToSend);
 
         if (sendResponse is null || !sendResponse.Successful)
         {
             return BadRequest(sendResponse?.ErrorMessages);
         }
 
-        return Ok("Email Successfully Sent!");
+        var mail = await _mailRepository.AddMail(mailToSend);
+
+        // return Ok("Email Successfully Sent!");
+        return CreatedAtAction(nameof(GetSavedMail), new {id = mail.Id}, mail);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetSavedMail(int id)
+    {
+        var mail  = await _mailRepository.GetMail(id);
+
+        return Ok(mail);
+    }
+
+    private Mail MapToMailObject(SendMailDto sendMailDto)
+    {
+        var mail = new Mail
+        {
+            Name = sendMailDto.Name,
+            Email = sendMailDto.Email,
+            RecipientEmail = sendMailDto.RecipientEmail,
+            Body = sendMailDto.Body
+        };
+        
+        if (sendMailDto.Subject is not null)
+        {
+            mail.Subject = sendMailDto.Subject;
+        }
+
+        return mail;
     }
 }
