6aa27f23cbeb072da814a53de1bedc0a89f3d2d2
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-06-03 10:41:46 +0300

    Add id property for mail model

--- SendMail/Models/Mail.cs
+++ SendMail/Models/Mail.cs
@@ -2,6 +2,7 @@ namespace SendMail.Models;
 
 public class Mail
 {
+    public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string RecipientEmail { get; set; } = string.Empty;
