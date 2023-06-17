ad188125f84758166af95f6f595b0ce960ce4106
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-06-18 01:09:49 +0300

    Add contacts model and dto

---
+++ SendMail/Models/Contact.cs
@@ -0,0 +1,9 @@
+namespace SendMail.Models;
+
+public class Contact
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+}
---
+++ SendMail/Models/ContactDto.cs
@@ -0,0 +1,8 @@
+namespace SendMail.Models;
+
+public class ContactDto
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+}
