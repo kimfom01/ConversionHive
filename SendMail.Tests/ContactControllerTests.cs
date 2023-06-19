d66fe04c706688946250c5586f95fe09d144d4c2
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-06-19 03:06:28 +0300

    Add unit tests for contact controller actions

---
+++ SendMail.Tests/ContactControllerTests.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SendMail.Controllers;
+using SendMail.Models;
+using SendMail.Repository;
+
+namespace SendMail.Tests;
+
+public class ContactControllerTests
+{
+    private readonly Mock<IUnitOfWork> _unitOfWork;
+    private readonly Mock<IMapper> _mapper;
+    private readonly ContactController _contactController;
+
+    public ContactControllerTests()
+    {
+        _unitOfWork = new Mock<IUnitOfWork>();
+        _mapper = new Mock<IMapper>();
+        _contactController = new ContactController(_unitOfWork.Object, _mapper.Object);
+    }
+
+    [Fact]
+    public async void PostContact_WhenCalled_ReturnsCreatedAtAction()
+    {
+        _mapper.Setup(m => m.Map<Contact>(It.IsAny<ContactDto>())).Returns(new Mock<Contact>().Object);
+        _unitOfWork.Setup(u => u.Contacts.AddItem(It.IsAny<Contact>())).ReturnsAsync(new Mock<Contact>().Object);
+
+        var result = await _contactController.PostContact(new Mock<ContactDto>().Object);
+
+        Assert.IsType<CreatedAtActionResult>(result);
+    }
+
+    [Fact]
+    public async void PostContact_WhenCalled_ReturnsBadRequest()
+    {
+        var result = await _contactController.PostContact(null);
+
+        Assert.IsType<BadRequestResult>(result);
+    }
+
+    [Fact]
+    public async void GetContact_WhenCalled_ReturnsOkResult()
+    {
+        _unitOfWork.Setup(u => u.Contacts.GetItem(It.IsAny<int>())).ReturnsAsync(new Mock<Contact>().Object);
+        _mapper.Setup(m => m.Map<ContactDto>(It.IsAny<ContactDto>())).Returns(new Mock<ContactDto>().Object);
+
+        var result = await _contactController.GetContact(It.IsAny<int>());
+
+        Assert.IsType<OkObjectResult>(result);
+    }
+
+    [Fact]
+    public async void GetContact_WhenCalled_ReturnsNotFoundResult()
+    {
+        _unitOfWork.Setup(u => u.Contacts.GetItem(It.IsAny<int>())).ReturnsAsync(() => null);
+
+        var result = await _contactController.GetContact(It.IsAny<int>());
+
+        Assert.IsType<NotFoundResult>(result);
+    }
+}
