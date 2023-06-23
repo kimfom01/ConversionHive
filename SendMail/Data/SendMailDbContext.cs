08832421732f7c0482cdc4ace33b4bf103836daa
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-06-23 17:35:18 +0300

    Add contacts as db set in db contexts

--- SendMail/Data/SendMailDbContext.cs
+++ SendMail/Data/SendMailDbContext.cs
@@ -6,6 +6,7 @@ namespace SendMail.Data;
 public class SendMailDbContext : DbContext
 {
     public DbSet<Mail> Mails { get; set; }
+    public DbSet<Contact> Contacts { get; set; }
 
     public SendMailDbContext(DbContextOptions<SendMailDbContext> options) : base(options)
     {
