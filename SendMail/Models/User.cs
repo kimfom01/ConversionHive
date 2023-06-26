3d2859b2295c0e787014b106ffda684d4849c327
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-06-26 15:27:35 +0300

    Add user model and dto for authentication and authorization

---
+++ SendMail/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace SendMail.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string EmailAddress { get; set; }
+    public string PasswordHash { get; set; }
+}
