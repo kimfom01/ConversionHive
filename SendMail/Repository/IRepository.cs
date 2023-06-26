41f63fd0854f565ff8b9da02864472fe7f299d6d
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-06-26 16:42:47 +0300

    Extend repository interface

--- SendMail/Repository/IRepository.cs
+++ SendMail/Repository/IRepository.cs
@@ -5,6 +5,7 @@ namespace SendMail.Repository;
 public interface IRepository<T>
 {
     Task<T?> GetItem(int id);
+    Task<T?> GetItem(Expression<Func<T, bool>> expression);
     Task<IQueryable<T>> GetItems(Expression<Func<T, bool>> expression);
     Task<T> AddItem(T item);
     Task AddItems(IEnumerable<T> items);
