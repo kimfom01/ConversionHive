3120fca0a405c7ca97c536ebecaaf9e10af41be1
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-06-26 16:43:08 +0300

    Add user dbset to db context

--- SendMail/Data/SendMailDbContext.cs
+++ SendMail/Data/SendMailDbContext.cs
@@ -7,6 +7,7 @@ public class SendMailDbContext : DbContext
 {
     public DbSet<Mail> Mails { get; set; }
     public DbSet<Contact> Contacts { get; set; }
+    public DbSet<User> Users { get; set; }
 
     public SendMailDbContext(DbContextOptions<SendMailDbContext> options) : base(options)
     {
