4704f4163f40d8e19159fd27b0086844dd2e15c5
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-06-30 21:17:20 +0300

    Add mail service to reduce code in controller

---
+++ SendMail/Services/IMailService.cs
@@ -0,0 +1,9 @@
+﻿using SendMail.Models;
+
+namespace SendMail.Services;
+
+public interface IMailService
+{
+    Task<Mail?> SendMail(MailDto sendMailDto);
+    Task<MailDto?> GetSavedMail(int id);
+}
---
+++ SendMail/Services/MailService.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using SendMail.Models;
+using SendMail.Repository;
+
+namespace SendMail.Services;
+
+public class MailService : IMailService
+{
+    private readonly IMailer _mailer;
+    private readonly IMapper _mapper;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public MailService(IMailer mailer, IMapper mapper, IUnitOfWork unitOfWork)
+    {
+        _mailer = mailer;
+        _mapper = mapper;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<MailDto?> GetSavedMail(int id)
+    {
+        var mail = await _unitOfWork.Mails.GetItem(id);
+
+        var mailDto = _mapper.Map<MailDto>(mail);
+
+        return mailDto;
+    }
+
+    public async Task<Mail?> SendMail(MailDto sendMailDto)
+    {
+        var mailToSend = _mapper.Map<Mail>(sendMailDto);
+
+        var success = await _mailer.SendMail(mailToSend);
+
+        if (!success)
+        {
+            return null;
+        }
+
+        var mail = await _unitOfWork.Mails.AddItem(mailToSend);
+        await _unitOfWork.SaveChangesAsync();
+
+        return mail;
+    }
+}
