6fd38bdac93e66a62d8d07cbb59c136c331cdf04
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-08-18 10:55:36 +0300

    Create model for registration

---
+++ FrontEnd/ConversionHive/ConversionHive/Models/RegisterModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ConversionHive.Models;
+
+public class RegisterModel
+{
+    [JsonPropertyName("firstName")]
+    public string FirstName { get; set; }
+    [JsonPropertyName("lastName")]
+    public string LastName { get; set; }
+    [JsonPropertyName("emailAddress")]
+    public string EmailAddress { get; set; }
+    [JsonPropertyName("password")]
+    public string Password { get; set; }
+}
