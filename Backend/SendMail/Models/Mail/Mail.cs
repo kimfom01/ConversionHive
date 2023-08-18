c49bf96ca71f071ef14ca5c215feb2222aacc996
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-08-18 08:53:44 +0300

    Update mail models with list of receivers

--- Backend/SendMail/Models/Mail/Mail.cs
+++ Backend/SendMail/Models/Mail/Mail.cs
@@ -1,3 +1,5 @@
+using SendMail.Models.ContactModels;
+
 namespace SendMail.Models.Mail;
 
 public class Mail
@@ -5,7 +7,9 @@ public class Mail
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Sender { get; set; } = string.Empty;
-    public string Receiver { get; set; } = string.Empty;
+    //public string Receiver { get; set; } = string.Empty;
     public string? Subject { get; set; }
     public string Body { get; set; } = string.Empty;
+
+    public List<ReceiverContactDto>? Receivers { get; set; }
 }
--- Backend/SendMail/Models/Mail/MailDto.cs
+++ Backend/SendMail/Models/Mail/MailDto.cs
@@ -1,10 +1,15 @@
+using SendMail.Models.ContactModels;
+
 namespace SendMail.Models.Mail;
 
 public class MailDto
 {
+    public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Sender { get; set; } = string.Empty;
-    public string Receiver { get; set; } = string.Empty;
+    //public string Receiver { get; set; } = string.Empty;
     public string? Subject { get; set; }
     public string Body { get; set; } = string.Empty;
+
+    public List<ReceiverContactDto>? Receivers { get; set; }
 }
