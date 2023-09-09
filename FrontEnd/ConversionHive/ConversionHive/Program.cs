b2cac366d0c23b65846857922e9247b98d737f97
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-09-09 18:39:38 +0300

    Add auth services to service container

--- FrontEnd/ConversionHive/ConversionHive/Program.cs
+++ FrontEnd/ConversionHive/ConversionHive/Program.cs
@@ -1,4 +1,7 @@
+using Blazored.LocalStorage;
 using ConversionHive;
+using ConversionHive.Services;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +9,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7214/api/User/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7214/api/") });
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 
 await builder.Build().RunAsync();
