5718adff3e4eeb96617c66a29ac446ec89c21697
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-09-09 18:31:21 +0300

    Add login model

---
+++ FrontEnd/ConversionHive/ConversionHive/Models/LoginModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ConversionHive.Models;
+
+public class LoginModel
+{
+    [JsonPropertyName("emailAddress")]
+    public string EmailAddress { get; set; }
+
+    [JsonPropertyName("password")]
+    public string Password { get; set; }
+}
