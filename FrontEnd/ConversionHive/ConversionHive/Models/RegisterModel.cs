501f3174e4dbf15b39e79cebc48425ff7b13ba24
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-09-09 18:34:24 +0300

    Modify register model for json serializer

--- FrontEnd/ConversionHive/ConversionHive/Models/RegisterModel.cs
+++ FrontEnd/ConversionHive/ConversionHive/Models/RegisterModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace ConversionHive.Models;
 
@@ -6,10 +7,14 @@ public class RegisterModel
 {
     [JsonPropertyName("firstName")]
     public string FirstName { get; set; }
+
     [JsonPropertyName("lastName")]
     public string LastName { get; set; }
+
     [JsonPropertyName("emailAddress")]
+    [EmailAddress]
     public string EmailAddress { get; set; }
+
     [JsonPropertyName("password")]
     public string Password { get; set; }
 }
