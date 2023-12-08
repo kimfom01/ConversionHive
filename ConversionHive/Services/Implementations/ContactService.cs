a6aa82ba9eb9a2598eed711e6d5cf085e9b4f82b
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-12-09 01:26:18 +0300

    Update process of getting id from claim in process contact

--- ConversionHive/Services/Implementations/ContactService.cs
+++ ConversionHive/Services/Implementations/ContactService.cs
@@ -12,7 +12,11 @@ public class ContactService : IContactService
     private readonly IMapper _mapper;
     private readonly IJwtProcessor _jwtProcessor;
 
-    public ContactService(ICsvService csvService, IUnitOfWork unitOfWork, IMapper mapper, IJwtProcessor jwtProcessor)
+    public ContactService(
+        ICsvService csvService,
+        IUnitOfWork unitOfWork,
+        IMapper mapper,
+        IJwtProcessor jwtProcessor)
     {
         _csvService = csvService;
         _unitOfWork = unitOfWork;
@@ -20,11 +24,16 @@ public class ContactService : IContactService
         _jwtProcessor = jwtProcessor;
     }
 
-    public async Task<IEnumerable<CreateContactResponseDto>?> ProcessContacts(string authorization, Stream fileStream)
+    public async Task<IEnumerable<CreateContactResponseDto>?>
+        ProcessContacts(string authorization, Stream fileStream)
     {
-        var id = _jwtProcessor.ExtractIdFromJwt(authorization);
+        // TODO: Create enum to hold claim types for different entities??
+        var claim = _jwtProcessor.ExtractClaimFromJwt(authorization, "Id"); 
 
-        var contactDtos = _csvService.ProcessCsv<CreateContactDto>(fileStream);
+        var id = int.Parse(claim.Value);
+
+        var contactDtos =
+            _csvService.ProcessCsv<CreateContactDto>(fileStream);
 
         if (contactDtos is null)
         {
@@ -38,7 +47,8 @@ public class ContactService : IContactService
         await _unitOfWork.Contacts.AddItems(contacts);
         await _unitOfWork.SaveChangesAsync();
 
-        var createContactResponseDtos = _mapper.Map<IEnumerable<CreateContactResponseDto>>(contacts);
+        var createContactResponseDtos = 
+            _mapper.Map<IEnumerable<CreateContactResponseDto>>(contacts);
 
         return createContactResponseDtos;
     }
