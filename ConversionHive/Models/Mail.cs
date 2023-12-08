ea8028da2ddff571cf6aa8c8ef007be8cb36dcc4
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-12-09 02:28:50 +0300

    Update mail model

---
+++ ConversionHive/Models/Mail.cs
@@ -0,0 +1,8 @@
+namespace ConversionHive.Models;
+
+public class Mail
+{
+    public string? Subject { get; set; }
+    public string Body { get; set; }
+    public string RecipientEmail { get; set; }
+}
