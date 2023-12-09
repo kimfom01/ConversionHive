a379a614d466acb19d8dc192359713c0b51a172d
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-12-09 16:46:41 +0300

    Add custom dto for contact csv file

---
+++ ConversionHive/Dtos/ContactDto/CreateContactCsvDto.cs
@@ -0,0 +1,8 @@
+namespace ConversionHive.Dtos.ContactDto;
+
+public class CreateContactCsvDto
+{
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string EmailAddress { get; set; }
+}
