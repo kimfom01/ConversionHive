908b918914d3b75e688605de0f09b6e8d525e759
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-12-09 16:46:08 +0300

    Add company to db context

--- ConversionHive/Data/SendMailDbContext.cs
+++ ConversionHive/Data/SendMailDbContext.cs
@@ -7,6 +7,7 @@ public class SendMailDbContext : DbContext
 {
     public DbSet<Contact> Contacts { get; set; }
     public DbSet<User> Users { get; set; }
+    public DbSet<Company> Companies { get; set; }
 
     public SendMailDbContext(DbContextOptions<SendMailDbContext> options) : base(options)
     {
