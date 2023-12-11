efd9b2ede0b606c30023bde5ff678fd277381c76
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-12-12 02:46:00 +0300

    Add recipient name

--- ConversionHive/Models/Mail.cs
+++ ConversionHive/Models/Mail.cs
@@ -5,4 +5,5 @@ public class Mail
     public string? Subject { get; set; }
     public string Body { get; set; }
     public string RecipientEmail { get; set; }
+    public string RecipientName { get; set; }
 }
