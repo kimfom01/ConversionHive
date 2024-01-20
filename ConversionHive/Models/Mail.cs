b585c863e10e8ffeaf95b848731f0caa977490f5
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-01-20 23:02:06 +0300

    Make all fields nullable

--- ConversionHive/Models/Mail.cs
+++ ConversionHive/Models/Mail.cs
@@ -3,9 +3,9 @@ namespace ConversionHive.Models;
 public class Mail
 {
     public string? Subject { get; set; }
-    public string Body { get; set; }
-    public string RecipientEmail { get; set; }
-    public string RecipientName { get; set; }
+    public string? Body { get; set; }
+    public string? RecipientEmail { get; set; }
+    public string? RecipientName { get; set; }
     public string? ContactEmail { get; set; }
     public string? ContactName { get; set; }
 }
